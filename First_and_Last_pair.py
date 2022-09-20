63479f7e28ef1b6873ca6b6be73dae0589ca94f3
YuvaRamyaSri <97425224+YuvaRamyaSri@users.noreply.github.com>
2022-09-20 08:58:58 +0530

    n=int(input()) l=list(map(int,input().split())) p=[] for i in range(n//2+1):     p.append(l[i]) q=[] for i in range(n-1,n//2,-1):     q.append(l[i]) i=0 j=0 while(i<len(p) or j<len(q)):     if(i<len(p)):         print(p[i],end=" ")         i+=1     if(j<len(q)):         print(q[j],end=" ")         j+=1 if(n%2!=0):     print(0)

--- First_and_Last_pair.py
+++ First_and_Last_pair.py
@@ -1,19 +1,19 @@
 n=int(input())
 l=list(map(int,input().split()))
-o=[]
-r=[]
+p=[]
 for i in range(n//2+1):
-    o.append(l[i])
+    p.append(l[i])
+q=[]
 for i in range(n-1,n//2,-1):
-    r.append(l[i])
+    q.append(l[i])
 i=0
 j=0
-while(i<len(o) or j<len(r)):
-    if(i<len(o)):
-        print(o[i],end=" ")
+while(i<len(p) or j<len(q)):
+    if(i<len(p)):
+        print(p[i],end=" ")
         i+=1
-    if(j<len(r)):
-        print(r[j],end=' ')
+    if(j<len(q)):
+        print(q[j],end=" ")
         j+=1
-if n%2!=0:
-    print('0')
+if(n%2!=0):
+    print(0)
