18d5282907a6b20e18ff7c9d7dba644e0ec6407c
YuvaRamyaSri <97425224+YuvaRamyaSri@users.noreply.github.com>
2022-09-20 23:06:45 +0530

    n=int(input()) l=list(map(int,input().split())) m=0 c=0 f=0 d=1000 r=[] for i in l:     c=l.count(i)     if(i==c):         if(i not in r):             r.append(i)             f+=1 print(f)

---
+++ value_same_as_count.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=list(map(int,input().split()))
+m=0
+c=0
+f=0
+d=1000
+r=[]
+for i in l:
+    c=l.count(i)
+    if(i==c):
+        if(i not in r):
+            r.append(i)
+            f+=1
+print(f)
