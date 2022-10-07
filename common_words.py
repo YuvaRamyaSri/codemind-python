fd7fdc3f772896a5902b3ce0a14a0130cd1118f5
YuvaRamyaSri <97425224+YuvaRamyaSri@users.noreply.github.com>
2022-10-07 10:14:10 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() a=list(s1.split(" ")) b=list(s2.split(" ")) c=[] for i in b:     if i in a and i not in c:         c.append(i) print(" ".join(c))

--- common_words.py
+++ common_words.py
@@ -1,7 +1,11 @@
-n=input().lower().replace(" "," ")
-m=input().lower().replace(" "," ")
-n=n.split()
-m=m.split()
-for i in m:
-    if(i!=" " and i in n):
-        print(i,end=" ")
+s1=input()
+s2=input()
+s1=s1.lower()
+s2=s2.lower()
+a=list(s1.split(" "))
+b=list(s2.split(" "))
+c=[]
+for i in b:
+    if i in a and i not in c:
+        c.append(i)
+print(" ".join(c))
