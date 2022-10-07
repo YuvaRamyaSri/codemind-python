63b0142c0ffd51a7aadc37dc4566c5c91793150d
YuvaRamyaSri <97425224+YuvaRamyaSri@users.noreply.github.com>
2022-10-07 10:35:36 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() p=[] for i in s2:     if i in s1 and i not in p and i!=" ":         p.append(i) for i in s1:     if i in s2 and i not in p and i!=" ":         p.append(i) p=sorted(p) print(len(p))

--- common_characters_-II.py
+++ common_characters_-II.py
@@ -1,10 +1,13 @@
-n=input().lower().replace(" "," ")
-m=input().lower().replace(" "," ")
-c=0
+s1=input()
+s2=input()
+s1=s1.lower()
+s2=s2.lower()
 p=[]
-for i in n:
-    for j in m:
-        if(i==j and i!=" " and i not in p):
-            p.append(i)
-            c+=1
-print(c)
+for i in s2:
+    if i in s1 and i not in p and i!=" ":
+        p.append(i)
+for i in s1:
+    if i in s2 and i not in p and i!=" ":
+        p.append(i)
+p=sorted(p)   
+print(len(p))
