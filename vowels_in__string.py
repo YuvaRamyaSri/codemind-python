b3d80c12f06c11faa64e913e50ddc91dd76c6d28
YuvaRamyaSri <97425224+YuvaRamyaSri@users.noreply.github.com>
2022-10-14 22:29:13 +0530

    s=input() v="aeiouAEIOU" b=[] for i in s:     if i in v and i not in b:         b.append(i) print(*b)

--- vowels_in__string.py
+++ vowels_in__string.py
@@ -1,11 +1,7 @@
 s=input()
-v="aeoiuAEIOU"
+v="aeiouAEIOU"
 b=[]
-f=0
 for i in s:
     if i in v and i not in b:
         b.append(i)
-        f=1
-        print(i,end=" ")
-if(f==0):
-    print(-1)
+print(*b)
